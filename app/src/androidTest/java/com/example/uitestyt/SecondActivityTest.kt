80c6c5cace66413af21859494924d9e137826ea9
Andrew <ext.akirepko@tinkoff.ru>
2023-03-09 13:56:38 +0300

    Добавил еще два кейса

--- app/src/androidTest/java/com/example/uitestyt/SecondActivityTest.kt
+++ app/src/androidTest/java/com/example/uitestyt/SecondActivityTest.kt
@@ -2,8 +2,7 @@ package com.example.uitestyt
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Rule
@@ -18,6 +17,21 @@ class SecondActivityTest{
     fun checkingActivity(){
         onView(withId(R.id.second_activity_view))
             .check(matches(isDisplayed()))
+    }
+    @Test
+    fun test_visibility_of_TextView_and_button(){
+        onView(withId(R.id.tvSecondActivity))
+            .check(matches(isDisplayed()))
+
+        onView(withId(com.example.uitestyt.R.id.btnSecondActivity))
+            .check(matches(isDisplayed()))
+    }
+    @Test
+    fun test_visibillity_textView(){
+        onView(withId(R.id.tvSecondActivity))
+            .check(matches(
+                withText(R.string.second_activity)
+            ))
     }
 
 }
